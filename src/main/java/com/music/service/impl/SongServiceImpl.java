b3e5d488e4e75ccf5f9c1a206c1abb9998653c3b
OULrx <tavillexback@outlook.com>
2026-07-16 15:09:45 +0800

    first

--- src/main/java/com/music/service/impl/SongServiceImpl.java
+++ src/main/java/com/music/service/impl/SongServiceImpl.java
@@ -1,7 +1,6 @@
 package com.music.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.music.common.R;
 import com.music.config.FileUploadConfig;
